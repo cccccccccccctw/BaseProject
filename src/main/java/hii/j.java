7da3b5f2b4f270a7e0d9c727374e1af4ccb3e46e
PC234 <cccccccccccctw@gmail.com>
2017-12-08 20:04:43 +0800

    hi

---
+++ src/main/java/hii/j.java
@@ -0,0 +1,12 @@
+package hii;
+
+public class j {
+    public static int getI(){
+        int i=1;
+        return i;
+    }
+
+    public static void main(){
+        System.out.println(getI());
+    }
+}
---
+++ src/test/java/hii/jTest.java
@@ -0,0 +1,13 @@
+package hii;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class jTest {
+    @Test
+    public void main() throws Exception {
+
+    }
+
+}
